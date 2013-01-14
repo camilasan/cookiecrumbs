3ec73c5f46d7f9c834bb9bdb3f55239d4213b3a8
Troels Lenda <troelslenda@gmail.com>
2013-01-14 13:45:24 +0100

    Adding library dependency to jquery.cookie.js

--- cookie_crumbs_config.js
+++ cookie_crumbs_config.js
@@ -11,7 +11,8 @@
         breadCrumbSelector: '.' + settings.cookie_crumbs.class_name,
         maxDepth: parseInt(settings.cookie_crumbs.items_to_show),
         debug: false,
-        cookieOptions: {path: "/"}
+        cookieOptions: {path: "/"},
+        inheritLandingCrumbs: false
       });
     }
   };
