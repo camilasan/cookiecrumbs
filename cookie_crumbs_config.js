2fcc211c8e9c0334b9636a863ac4fe63b03c4380
Troels Lenda <troelslenda@gmail.com>
2013-05-31 08:32:53 +0200

    Introducing possibility to override the title with the hook.

--- cookie_crumbs_config.js
+++ cookie_crumbs_config.js
@@ -6,8 +6,16 @@
 
   Drupal.behaviors.cookie_crumbs = {
     attach: function (context, settings) {
+      // If hook_cookie_crumbs_js_settings() sets title_override it will
+      // override the title.
+      if (settings.cookie_crumbs.title_override) {
+        var titleCallback = function(){
+          return settings.cookie_crumbs.title_override;
+        }
+      }
       // Attach the breadcrumbs.
       $.fn.hanselsRevenge({
+        titleCallback : titleCallback,
         breadCrumbSelector: '.' + settings.cookie_crumbs.class_name,
         maxDepth: parseInt(settings.cookie_crumbs.items_to_show),
         debug: false,
